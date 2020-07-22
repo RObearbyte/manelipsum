8d65150016f1a487fb29bd479ea773d896656ca2
Rares-Stefan Florea <rares.florea@saltandpepper.co>
2020-07-22 16:53:34 +0300

    staging test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src="https://media1.tenor.com/images/30f63957c087f8777140c3d1b7ef7520/tenor.gif?itemid=8226749" className="App-logo" alt="logo" />
         <p>
-          Fii pe faza, boss! Acuma incalzim valoarea!
+          Fii pe faza, boss! Acuma incalzim valoarea! -staging
         </p>
       </header>
     </div>
