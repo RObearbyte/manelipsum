eb756d689b0c7d6bb2c40dc86eff9614ed6d9f86
Rares-Stefan Florea <rares.florea@saltandpepper.co>
2020-07-27 23:59:58 +0300

    loader component - actual landing page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import './App.css';
+import loading from './components/Landing/Loading'
+import './stylesheets/main/App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="https://media1.tenor.com/images/30f63957c087f8777140c3d1b7ef7520/tenor.gif?itemid=8226749" className="App-logo" alt="logo" />
-        <p>
-          Fii pe faza, boss! Acuma incalzim valoarea! -staging
-        </p>
-      </header>
-    </div>
-  );
+      {loading()}
+    </div>);
 }
 
 export default App;
---
+++ src/components/Landing/Loading.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const loading = () => {
+    return (
+        <header className="App-header">
+            <img src="https://media1.tenor.com/images/30f63957c087f8777140c3d1b7ef7520/tenor.gif?itemid=8226749" className="App-logo" alt="logo" />
+            <p>
+                Fii pe faza, boss! Acuma incalzim valoarea!
+            </p>
+        </header>
+    )
+}
+
+export default loading;
