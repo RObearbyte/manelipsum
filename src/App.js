ef349c228b690baa627e78becec2a8fa2e2834bf
Rares-Stefan Florea <rares.florea@saltandpepper.co>
2020-07-31 23:54:03 +0300

    title component, conditional rendering for loading component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import loading from './components/Landing/Loading'
+import loading from './components/Loading/Loading'
+import title from './components/Title/Title'
 import './stylesheets/main/App.css';
 
 function App() {
   return (
     <div className="App">
-      {loading()}
+      <header className="App-header">
+        {title ? title : loading}
+      </header>
     </div>);
 }
 
---
+++ src/components/Title/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './title.css';
+
+const title = (
+    <div className="title">
+        <span className="text">
+            manelipsum
+            </span>
+    </div>
+)
+
+export default title;
