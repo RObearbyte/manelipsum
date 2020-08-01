5acb3652f7dc32f41280defb1ee7c6845e20565c
Rares-Stefan Florea <rares.florea@saltandpepper.co>
2020-08-02 00:03:35 +0300

    col test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import loading from './components/Loading/Loading'
 import title from './components/Title/Title'
 import SubmitButton from './components/SubmitButton/SubmitButton'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 import './stylesheets/main/App.css';
 
@@ -10,12 +12,16 @@ import './stylesheets/main/App.css';
 function App() {
   return (
     <Container fluid>
-      <div className="App">
-        <header className="App-header">
-          {title ? title : loading}
-        </header>
-        <SubmitButton />
-      </div>
+      <Row>
+        <Col xs={12} >
+          <div className="App">
+            <header className="App-header">
+              {title ? title : loading}
+            </header>
+            <SubmitButton />
+          </div>
+        </Col>
+      </Row>
     </Container>);
 }
 
